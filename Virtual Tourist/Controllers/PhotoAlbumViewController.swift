5786ae04461837576e22d252e3613ba0de78e2f4
alextaoultsides <alextaoultsides@gmail.com>
2018-05-16 23:53:39 -0400

    First Commit

---
+++ Virtual Tourist/Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by atao1 on 5/14/18.
+//  Copyright © 2018 atao. All rights reserved.
+//
+
+import Foundation
---
+++ Virtual Tourist/Model/DataController.swift
@@ -0,0 +1,9 @@
+//
+//  DataController.swift
+//  Virtual Tourist
+//
+//  Created by atao1 on 5/15/18.
+//  Copyright © 2018 atao. All rights reserved.
+//
+
+import Foundation
