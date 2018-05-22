a92c7a3cb513972578134ed41f3f68652a748a60
alextaoultsides <alextaoultsides@gmail.com>
2018-05-22 16:27:52 -0400

    Feat:  Displays 15 images from flickr for pin

---
+++ Virtual Tourist/View/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  Virtual Tourist
+//
+//  Created by atao1 on 5/20/18.
+//  Copyright © 2018 atao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+}
